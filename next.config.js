8c87cbb4799158af090cad6eec6feca0ec35bbcc
Sohini Das <sohinidas341@gmail.com>
2025-11-21 10:55:59 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // if you later host model on external domain, add it here
+};
+
+module.exports = nextConfig;
